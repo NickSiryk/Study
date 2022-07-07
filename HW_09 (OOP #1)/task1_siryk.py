036c2cb02e8d6ced40f7e5eca89c19268505f48d
Nick Siryk <siriknick@gmail.com>
2022-07-07 19:00:43 +0300

    comments added

--- HW_09 (OOP #1)/task1_siryk.py
+++ HW_09 (OOP #1)/task1_siryk.py
@@ -1,10 +1,9 @@
+'''
+   A simple greeting program
+'''
+
 class Person:
     def __init__(self, name, last_name, age):
-        '''
-        :param name:
-        :param last_name:
-        :param age:
-        '''
         self.name = name
         self.last_name = last_name
         self.age = age
